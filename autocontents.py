2d7673c171b645ea3fff0e8b1f10840356540cd0
Alphonse Mariya <alphonse.mariya@hotmail.com>
2018-10-15 17:03:20 +0200

    Update script to include Github stats

--- autocontents.py
+++ autocontents.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 # Load all essential statistics from AWS SAM including almost-exact number of Lambdas as well as metadata.
 
 import urllib.request
@@ -34,19 +35,30 @@ def pullstatistics(stamp):
 			stf.close()
 
 		for app in struct["applications"]:
+			if not "homePageUrl" in app.keys():
+				app["homePageUrl"] = ""
+
+			if "github.com" in app["homePageUrl"]:
+				github_stats(app, app["homePageUrl"].replace("github.com", "api.github.com/repos"))
+
 			print("- {} / {}".format(app["name"], app["id"]))
 			print("  by: {}".format(app["publisherAlias"]))
 			print("  deployments: {}".format(app["deploymentCount"]))
 			print("  labels: {}".format(",".join(app["labels"])))
+			print("  homepage: {}".format(app["homePageUrl"]))
 
 			fields = []
-			fields.append(app["name"])
-			fields.append(app["id"])
-			fields.append(app["publisherAlias"])
-			fields.append(app["deploymentCount"])
-			fields.append(app["homePageUrl"])
-			fields.append(",".join(app["labels"]))
-			fields.append(app["description"])
+			fields.append(app.get("name"))
+			fields.append(app.get("id"))
+			fields.append(app.get("publisherAlias"))
+			fields.append(app.get("deploymentCount"))
+			fields.append(app.get("homePageUrl", ""))
+			fields.append(app.get("stars", ""))
+			fields.append(app.get("watchers", ""))
+			fields.append(app.get("forks", ""))
+			fields.append(app.get("language", ""))
+			fields.append(",".join(app.get("labels")))
+			fields.append(app.get("description"))
 
 			w.writerow(fields)
 
@@ -54,5 +66,20 @@ def pullstatistics(stamp):
 			break
 
 	f.close()
+
+def github_stats(app, link):
+	try:
+		print("-> fetch github: {}".format(link))
+
+		resource = urllib.request.urlopen(link)
+		gh_content = resource.read().decode("utf-8")
+		struct = json.loads(gh_content)
+
+		app["stars"] = struct["stargazers_count"]
+		app["watchers"] = struct["subscribers_count"]
+		app["forks"] = struct["forks_count"]
+		app["language"] = struct["language"]
+	except:
+		print("-> fetch github: ERROR")
 
 pullstatistics(datetime.date.isoformat(datetime.date.today()))
